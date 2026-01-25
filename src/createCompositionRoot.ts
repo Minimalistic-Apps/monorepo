0f65cfb83870c449bbdca220ee38e501f891d59f
hank.milliken94@protonmail.com <Hank Milliken>
2026-01-26 00:47:46 +0100

    fix: globalThis

--- src/createCompositionRoot.ts
+++ src/createCompositionRoot.ts
@@ -5,7 +5,8 @@ import { createFetchAverageRates } from './services/fetchAverageRates.js';
 
 export const createCompositionRoot = () => {
     const fetchDeps = {
-        fetch: globalThis.fetch,
+        // Important to be wrapped to preserve the correct `this` context
+        fetch: (input: RequestInfo | URL, init?: RequestInit) => globalThis.fetch(input, init),
     };
 
     const fetchCoingeckoRates = createFetchCoingeckoRates(fetchDeps);
